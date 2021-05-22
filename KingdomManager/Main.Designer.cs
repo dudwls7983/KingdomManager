66db0a84348636841dc7af6c412fdd1cee3e910b
영진 서 <dudwls7983@naver.com>
2021-05-22 20:21:43 +0900

    Update

---
+++ KingdomManager/Main.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace KingdomManager
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.linkButton = new System.Windows.Forms.Button();
+            this.appPlayerList = new System.Windows.Forms.ComboBox();
+            this.targetList = new System.Windows.Forms.ComboBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(561, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // linkButton
+            // 
+            this.linkButton.Location = new System.Drawing.Point(123, 58);
+            this.linkButton.Name = "linkButton";
+            this.linkButton.Size = new System.Drawing.Size(100, 23);
+            this.linkButton.TabIndex = 2;
+            this.linkButton.Text = "링크";
+            this.linkButton.UseVisualStyleBackColor = true;
+            this.linkButton.Click += new System.EventHandler(this.OnLinkButtonClicked);
+            // 
+            // appPlayerList
+            // 
+            this.appPlayerList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.appPlayerList.FormattingEnabled = true;
+            this.appPlayerList.Location = new System.Drawing.Point(123, 6);
+            this.appPlayerList.Name = "appPlayerList";
+            this.appPlayerList.Size = new System.Drawing.Size(100, 20);
+            this.appPlayerList.TabIndex = 4;
+            this.appPlayerList.SelectedIndexChanged += new System.EventHandler(this.OnAppPlayerListSelectionChanged);
+            // 
+            // targetList
+            // 
+            this.targetList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.targetList.FormattingEnabled = true;
+            this.targetList.Location = new System.Drawing.Point(123, 32);
+            this.targetList.Name = "targetList";
+            this.targetList.Size = new System.Drawing.Size(100, 20);
+            this.targetList.TabIndex = 5;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1560, 837);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.appPlayerList);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.linkButton);
+            this.tabPage1.Controls.Add(this.targetList);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1552, 811);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "일반";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1552, 811);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "생산";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "앱플레이어";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "타겟";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Main";
+            this.Text = "KingdomManager";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button linkButton;
+        private System.Windows.Forms.ComboBox appPlayerList;
+        private System.Windows.Forms.ComboBox targetList;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ KingdomManager/Main.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace KingdomManager
+{
+    public partial class Main : Form
+    {
+        #region Interop Function
+        // 프로그램의 최상위 핸들을 찾아주는 함수
+        [DllImport("User32", EntryPoint = "FindWindow")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        // 프로그램의 자식 핸들을 찾아주는 함수
+        [DllImport("user32")]
+        private static extern IntPtr FindWindowEx(IntPtr hWnd1, int hWnd2, string lp1, string lp2);
+
+        // 핸들로부터 이미지를 출력하는 함수
+        [DllImport("user32.dll")]
+        internal static extern bool PrintWindow(IntPtr hwnd, IntPtr hdcblt, int nFlags);
+
+        // 핸들에 메시지를 전달하는 함수
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, IntPtr lParam);
+        #endregion
+
+
+        #region const & readonly variables
+        public const int WM_LBUTTONDOWN = 0x201;
+        public const int WM_LBUTTONUP = 0x202;
+
+        readonly string[,] programList = { //{"ComboBox에 보여줄 이름", "프로세스 이름", "윈도우핸들이름", "윈도우핸들클래스" }, 
+            {"LD플레이어", "dnplayer", "RenderWindow", "TheRender" }
+        };
+        #endregion
+
+        #region Local Variables
+        public bool isLinked;
+        public IntPtr linkedHandle;
+        public int linkedWidth;
+        public int linkedHeight;
+        #endregion
+
+        public Main()
+        {
+            InitializeComponent();
+
+            #region Initialize
+            Unlink();
+            #endregion
+
+            #region 앱플레이어 리스트 추가
+            for (int i = 0; i < programList.GetLength(0); i++)
+            {
+                appPlayerList.Items.Add(programList[i, 0]);
+            }
+
+            appPlayerList.SelectedIndex = 0;
+            #endregion
+        }
+
+        #region 이벤트 Listner
+        private void OnLinkButtonClicked(object sender, EventArgs e)
+        {
+            if (isLinked)
+                Unlink();
+            else
+            {
+                if (Link() == false)
+                    return;
+
+                Bitmap bmp = PrintScreen();
+                if (bmp == null)
+                    return;
+
+                // 기존 이미지의 메모리 할당을 해제한다.
+                if (pictureBox1.Image != null)
+                    pictureBox1.Image.Dispose();
+
+                pictureBox1.Width = bmp.Width;
+                pictureBox1.Height = bmp.Height;
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void OnAppPlayerListSelectionChanged(object sender, EventArgs e)
+        {
+            targetList.Items.Clear();
+            Process[] processList = Process.GetProcesses();
+
+            foreach (Process process in processList)
+            {
+                String title = process.MainWindowTitle;
+                if (String.IsNullOrEmpty(title) == true || process.ProcessName.Equals(programList[appPlayerList.SelectedIndex,1]) == false)
+                    continue;
+
+                targetList.Items.Add(title);
+            }
+
+            if (targetList.Items.Count > 0)
+                targetList.SelectedIndex = 0;
+        }
+
+        #endregion
+
+        #region Public Functions
+        /// <summary>
+        /// 사용 후 반드시 Dispose로 메모리할당 해제해줘야 한다.
+        /// </summary>
+        /// <returns></returns>
+        public Bitmap PrintScreen()
+        {
+            if (linkedHandle == null)
+                return null;
+
+            Graphics graphics = Graphics.FromHwnd(linkedHandle);
+            if (graphics == null)
+                return null;
+
+            Rectangle rect = Rectangle.Round(graphics.VisibleClipBounds);
+            Bitmap bmp = new Bitmap(rect.Width, rect.Height);
+            linkedWidth = rect.Width;
+            linkedHeight = rect.Height;
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                IntPtr hdc = g.GetHdc();
+                PrintWindow(linkedHandle, hdc, 0x2);
+                g.ReleaseHdc(hdc);
+            }
+
+            return bmp;
+        }
+
+        public void ClickScreen(int x, int y)
+        {
+            if (isLinked == false || linkedHandle == IntPtr.Zero)
+                return;
+
+            IntPtr param = new IntPtr(x | (y << 16));
+            SendMessage(linkedHandle, WM_LBUTTONDOWN, 1, param);
+            SendMessage(linkedHandle, WM_LBUTTONUP, 0, param);
+        }
+        #endregion
+
+        #region Private Functions
+        private bool Link()
+        {
+            var item = targetList.SelectedItem;
+            if (item == null)
+                return false;
+
+            IntPtr parentHandle = FindWindow(null, item.ToString());
+            if (parentHandle == null)
+                return false;
+
+            IntPtr childHandle = FindWindowEx(parentHandle, 0, "RenderWindow", "TheRender");
+            if (childHandle == null)
+                return false;
+
+            isLinked = true;
+            linkedHandle = childHandle;
+            return true;
+        }
+
+        private void Unlink()
+        {
+            isLinked = false;
+            linkedHandle = IntPtr.Zero;
+        }
+        #endregion
+    }
+}
