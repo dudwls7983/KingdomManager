9f85449ae50198fb90d28ced21d1401f3b1f1f82
영진 서 <dudwls7983@naver.com>
2021-05-22 17:01:19 +0900

    Add ImageCapture

--- KingdomManager/Form1.Designer.cs
+++ KingdomManager/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(818, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(818, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "KingdomManager";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- KingdomManager/Form1.cs
+++ KingdomManager/Form1.cs
@@ -7,14 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace KingdomManager
 {
     public partial class Form1 : Form
     {
+        // 프로그램의 최상위 핸들을 찾아주는 함수
+        [DllImport("User32", EntryPoint = "FindWindow")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        // 프로그램의 자식 핸들을 찾아주는 함수
+        [DllImport("user32")]
+        private static extern IntPtr FindWindowEx(IntPtr hWnd1, int hWnd2, string lp1, string lp2);
+
+        // 핸들로부터 이미지를 출력하는 함수
+        [DllImport("user32.dll")]
+        internal static extern bool PrintWindow(IntPtr hwnd, IntPtr hdcblt, int nFlags);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string windowName = textBox1.Text;
+
+            IntPtr parentHandle = FindWindow(null, windowName);
+            if (parentHandle == null)
+                return;
+
+            IntPtr childHandle = FindWindowEx(parentHandle, 0, "RenderWindow", "TheRender");
+            if (childHandle == null)
+                return;
+
+            Graphics graphics = Graphics.FromHwnd(childHandle);
+            if (graphics == null)
+                return;
+
+            Rectangle rect = Rectangle.Round(graphics.VisibleClipBounds);
+            Bitmap bmp = new Bitmap(rect.Width, rect.Height);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                IntPtr hdc = g.GetHdc();
+                PrintWindow(childHandle, hdc, 0x2);
+                g.ReleaseHdc(hdc);
+            }
+
+            pictureBox1.Image = bmp;
         }
     }
 }
